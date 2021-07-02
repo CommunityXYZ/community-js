9dbdd4b3445cb0bfdd33b48950182354047f400a
Cedrik <cedrik.git@tryninja.io>
2021-07-02 12:34:35 -0500

    upgraded deps

--- build.js
+++ build.js
@@ -17,7 +17,7 @@ const runBuild = async (doClean = false) => {
     minify: false,
     bundle: true,
     platform: 'browser',
-    target: ['es2020','chrome58','firefox57','safari11','edge16'],
+    target: ['es2020','chrome58','firefox57','safari11'],
     outfile: './dist/community.js',
     sourcemap: 'inline'
   }).catch((e) => {
@@ -31,7 +31,7 @@ const runBuild = async (doClean = false) => {
     minify: true,
     bundle: true,
     platform: 'browser',
-    target: ['es2020','chrome58','firefox57','safari11','edge16'],
+    target: ['es2020','chrome58','firefox57','safari11'],
     outfile: './dist/community.min.js',
     sourcemap: 'inline'
   }).catch((e) => {
