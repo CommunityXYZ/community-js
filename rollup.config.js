c5d53404cd52e783894f296a0633cbd776955fe5
Cedrik <cedrik.git@tryninja.io>
2021-07-20 17:26:22 -0500

    rollup-require

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser'
 
 export default {
   input: 'src/community.ts',
   output: {
-    dir: 'lib',
+    file: process.env.MINIFY? 'lib/community.min.js' : 'lib/community.js',
     format: 'umd',
-    name: 'community'
+    name: 'Community'
   },
-  plugins: [typescript({lib: ["es5", "es6", "dom"], target: "es5"})]
+  plugins: process.env.MINIFY? [resolve(), typescript({lib: ["es5", "es6", "dom"], target: "esnext"}), terser()] : [resolve(), typescript({lib: ["es5", "es6", "dom"], target: "esnext"})]
 };
