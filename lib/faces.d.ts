065bde0f571b4161b020fb077381741888326d4a
Cedrik <cedrik.git@tryninja.io>
2021-07-20 17:52:13 -0500

    rollup esm/cjs test

---
+++ lib/faces.d.ts
@@ -0,0 +1,61 @@
+export interface StateInterface {
+    name: string;
+    ticker: string;
+    balances: BalancesInterface;
+    vault: VaultInterface;
+    votes: VoteInterface[];
+    roles: RoleInterface;
+    settings: Map<string, any>;
+}
+export interface RoleInterface {
+    [key: string]: string;
+}
+export interface BalancesInterface {
+    [key: string]: number;
+}
+export interface VaultInterface {
+    [key: string]: VaultParamsInterface[];
+}
+export interface VaultParamsInterface {
+    balance: number;
+    start: number;
+    end: number;
+}
+export interface ActionInterface {
+    input: InputInterface;
+    caller: string;
+}
+export interface InputInterface extends VoteInterface {
+    function: GetFunctionType | SetFunctionType;
+    cast?: string;
+}
+export interface VoteInterface {
+    status?: VoteStatus;
+    type?: VoteType;
+    id?: number;
+    totalWeight?: number;
+    recipient?: string;
+    target?: string;
+    qty?: number;
+    key?: string;
+    value?: any;
+    note?: string;
+    yays?: number;
+    nays?: number;
+    voted?: string[];
+    start?: number;
+    lockLength?: number;
+}
+export interface ResultInterface {
+    target: string;
+    balance: number;
+    role: string;
+}
+export interface TagInterface {
+    name: string;
+    value: string;
+}
+export declare type VoteStatus = 'active' | 'quorumFailed' | 'passed' | 'failed';
+export declare type VoteType = 'mint' | 'mintLocked' | 'burnVault' | 'indicative' | 'set';
+export declare type GetFunctionType = 'balance' | 'unlockedBalance' | 'vaultBalance' | 'role';
+export declare type SetFunctionType = 'transfer' | 'transferLocked' | 'vote' | 'propose' | 'finalize' | 'lock' | 'increaseVault' | 'unlock';
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,46 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser'
+import babel from "@rollup/plugin-babel";
 
-export default {
+export default [{
   input: 'src/community.ts',
   output: {
     file: process.env.MINIFY? 'lib/community.min.js' : 'lib/community.js',
     format: 'umd',
-    name: 'Community'
+    name: 'Community',
+    esModule: false,
+    exports: 'named',
+    sourcemap: true
   },
-  plugins: process.env.MINIFY? [typescript({lib: ["es5", "es6", "dom"], target: "esnext"}), resolve(), terser()] : [typescript({lib: ["es5", "es6", "dom"], target: "esnext"}), resolve()]
-};
+  plugins: process.env.MINIFY? [
+    typescript({lib: ["es5", "es6", "dom"], target: "esnext"}), 
+    resolve(), 
+    babel({
+      babelHelpers: 'bundled'
+    }),
+    terser()
+  ] 
+  : [
+    typescript(), 
+    resolve(),
+    babel({
+      babelHelpers: 'bundled'
+    }),
+  ]
+},
+{
+  input: 'src/community.ts',
+  plugins: [typescript(), resolve()],
+  output: [{
+    file: 'lib/index.es.js',
+    format: 'esm',
+    exports: 'named',
+    sourcemap: true,
+  }, {
+    file: 'lib/index.cjs.js',
+    format: 'cjs',
+    exports: 'named',
+    sourcemap: true
+  }]
+}];
