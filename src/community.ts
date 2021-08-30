8a0fc496b9e97b2afca777983e2430076d36f64e
Cedrik <cedrik.git@tryninja.io>
2021-08-29 22:11:38 -0500

    removed unnecesary logs

--- src/community.ts
+++ src/community.ts
@@ -541,10 +541,7 @@ export default class Community {
         this.createFee = +(this.createFeeUsd / arPrice).toFixed(5);
         this.txFee = +(this.txFeeUsd / arPrice).toFixed(5);
       }
-    } catch (e) {
-      console.log(e);
-      console.warn('Was not able to update the fees, please try again later');
-    }
+    } catch { }
 
     this.feesUpdatedAt = Date.now();
     this.feesCallInProgress = false;
@@ -727,9 +724,8 @@ export default class Community {
         { name: 'Action', value: 'propose' },
         {
           name: 'Message',
-          value: `Proposed ${pCopy.type === 'indicative' || pCopy.key === 'other' ? 'an' : 'a'} ${
-            pCopy.key || pCopy.type
-          } vote, value: ${pCopy.value}.`,
+          value: `Proposed ${pCopy.type === 'indicative' || pCopy.key === 'other' ? 'an' : 'a'} ${pCopy.key || pCopy.type
+            } vote, value: ${pCopy.value}.`,
         },
         { name: 'Community-ID', value: this.communityContract },
         { name: 'Service', value: 'CommunityXYZ' },
@@ -940,9 +936,9 @@ export default class Community {
       typeof window !== 'undefined'
         ? window
         : {
-            removeEventListener: (evName: string) => {},
-            addEventListener: (evName: string, callback: (e: any) => {}) => {},
-          };
+          removeEventListener: (evName: string) => { },
+          addEventListener: (evName: string, callback: (e: any) => {}) => { },
+        };
 
     async function walletConnect() {
       this.walletAddress = await this.arweave.wallets.getAddress();
